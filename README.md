e45a5fb03c2e29dd0110d1fdc2b1d0213e3909c9
Aa-thomas <Aartho95@gmail.com>
2022-09-01 09:47:56 -0400

    update

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 A portfolio site showcasing some projects I have made. Built with NodeJS and Express.
 
 <!-- to do -->
-<!-- 1. crop screenshots, add Headshot -->
-<!-- 2. complete data.json file -->
-<!-- 3. fix any bugs in earlier projects -->
+<!-- 1. crop screenshots -->
+<!-- 2. fix bugs in earlier code projects -->
--- app.js
+++ app.js
@@ -1,12 +1,50 @@
-const { application } = require('express');
 const express = require('express');
-const projects = require('./data.json');
+const projects = require('./data.json').projects;
 
 const app = express();
 
-app.set('view-engine', 'pug');
-
+app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
+
+/* Routes */
+app.get('/', (req, res) => {
+	res.render('index', { projects });
+});
+
+app.get('/about', (req, res) => {
+	res.render('about');
+});
+
+app.get('/project/:id', (req, res) => {
+	res.render('project', { projects });
+});
+
+/* ERROR HANDLERS */
+/* 404 handler to catch undefined and non-existent route requests */
+app.use((req, res, next) => {
+	const err = new Error();
+	err.status = 404;
+	err.message = `Oh no! It looks like the page you're looking for doesn't exist`;
+	console.log('404 error handler called', err);
+	res.status(404).render('page-not-found', { err });
+	next(err);
+});
+
+/* Global Error Handler */
+app.use((err, req, res, next) => {
+	if (err) {
+		console.log('Global error handler called', { err });
+	}
+	if (err.status === 404) {
+		res.status(404).render('page-not-found', { err });
+	} else {
+		err.status = err.status;
+		err.message =
+			err.message ||
+			`Oops! It looks like something went wrong on the server.`;
+		res.status(err.status || 500).render('error', { err });
+	}
+});
 
 app.listen(3000, () => {
 	console.log('The application is now running on localhost:3000');
